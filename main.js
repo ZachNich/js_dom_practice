9175bea1624f7eae0acfd298c3882d066b967947
Zach Nicholson <raiste.doctow@gmail.com>
2020-04-19 16:04:17 -0500

    added header, organized html, added title image

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-const firstHeaderEl = document.querySelector(".article__header");
+// EXERCISE REQUIRED JS, 
+// affects .article__header, .article__footer, and .dashed
 
-firstHeaderEl.textContent = 'Welcome to the Zach blog';
+// changing content of first header
 
-const allHeaderEl = document.querySelectorAll(".article__header");
+const firstHeader = document.querySelector(".article__header");
 
-for (let i = 0; i < allHeaderEl.length; i++) {
-    allHeaderEl[i].classList.add('important');
+firstHeader.textContent = 'Welcome to the Zach blog';
+
+// adding .important class to all headers
+
+const allHeader = document.querySelectorAll(".article__header");
+
+for (let i = 0; i < allHeader.length; i++) {
+    allHeader[i].classList.add('important');
 };
+
+// removing .dashed class
 
 const dashed = document.querySelector(".dashed");
 
 dashed.classList.remove('dashed');
 
-const footerEl = document.querySelector('.article__footer')
+// adding .goldenrod class to footer
 
-footerEl.classList.add('goldenrod')
+const footerArticle = document.querySelector('.article__footer')
+
+footerArticle.classList.add('goldenrod')
+
+// PERSONAL JS ADDED
+
+// checks which header nav button is current page and highlights it
+
+let navs = document.querySelectorAll('.nav__header a');
+
+for (i = 0; i < navs.length; i++) {
+    if (navs[i].href == window.location.href) {
+        navs[i].parentNode.classList.add('active');
+    };
+};
